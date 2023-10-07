07ceb71145fc6f8068678b29de3ed16b268bab43
Rafael <rafaelmarliere033@gmail.com>
2023-10-07 02:26:29 -0300

    ainda tenho que modularizar esse cara

--- main.cpp
+++ main.cpp
@@ -1,179 +1,186 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// Definição da estrutura de nó da lista circular duplamente encadeada
-typedef struct No {
-    int dado;
-    struct No* proximo;
-    struct No* anterior;
-} No;
-
-// Função para inicializar uma lista vazia
-No* inicializarLista() {
-    return NULL;
-}
-
-// Função para inserir um elemento no início da lista
-No* inserirNoInicio(No* lista, int valor) {
-    No* novoNo = (No*)malloc(sizeof(No));
-    novoNo->dado = valor;
-
-    if (lista == NULL) {
-        novoNo->proximo = novoNo;
-        novoNo->anterior = novoNo;
-    } else {
-        novoNo->proximo = lista;
-        novoNo->anterior = lista->anterior;
-        lista->anterior->proximo = novoNo;
-        lista->anterior = novoNo;
-    }
-
-    return novoNo;
-}
-
-// Função para inserir um elemento no final da lista
-No* inserirNoFinal(No* lista, int valor) {
-    No* novoNo = (No*)malloc(sizeof(No));
-    novoNo->dado = valor;
-
-    if (lista == NULL) {
-        novoNo->proximo = novoNo;
-        novoNo->anterior = novoNo;
-    } else {
-        novoNo->proximo = lista;
-        novoNo->anterior = lista->anterior;
-        lista->anterior->proximo = novoNo;
-        lista->anterior = novoNo;
-    }
-
-    return lista;
-}
-
-// Função para remover um elemento da lista com base em um valor específico
-No* removerElemento(No* lista, int valor) {
-    if (lista == NULL) {
-        return NULL;
-    }
-
-    No* atual = lista;
-    do {
-        if (atual->dado == valor) {
-            No* proximoNo = atual->proximo;
-            No* noAnterior = atual->anterior;
-            noAnterior->proximo = proximoNo;
-            proximoNo->anterior = noAnterior;
-
-            if (atual == lista) {
-                lista = proximoNo;
-            }
-
-            free(atual);
-            break;
-        }
-
-        atual = atual->proximo;
-    } while (atual != lista);
-
-    return lista;
-}
-
-// Função para buscar um elemento na lista
-No* buscarElemento(No* lista, int valor) {
-    No* atual = lista;
-    if (atual == NULL) {
-        return NULL;
-    }
-
-    do {
-        if (atual->dado == valor) {
-            return atual;
-        }
-
-        atual = atual->proximo;
-    } while (atual != lista);
-
-    return NULL;
-}
-
-// Função para exibir todos os elementos da lista
-void imprimirLista(No* lista) {
-    No* atual = lista;
-    if (atual == NULL) {
-        printf("Lista vazia.\n");
-        return;
-    }
-
-    printf("Lista Atual: ");
-    do {
-        printf("%d ", atual->dado);
-        atual = atual->proximo;
-    } while (atual != lista);
-
-    printf("\n");
-}
+#include <iostream>
+#include <conio.h>
 
 int main() {
-    No* lista = inicializarLista();
-    int escolha, valor;
+    struct LISTA {
+        int numero;
+        LISTA *proximo;
+        LISTA *anterior;
+    };
+
+    LISTA *inicio = nullptr;
+    LISTA *fim = nullptr;
+    LISTA *auxiliar = nullptr;
+
+    int opcao, numero, achou;
 
     do {
-        printf("X------------------------------X\n");
-        printf("| 1 - Inserir no Início         |\n");
-        printf("| 2 - Inserir no Final          |\n");
-        printf("| 3 - Remover Elemento          |\n");
-        printf("| 4 - Buscar Elemento           |\n");
-        printf("| 5 - Exibir Lista              |\n");
-        printf("| 6 - Sair                      |\n");
-        printf("X------------------------------X\n");
+        std::cout << "\nMenu de opções\n";
+        std::cout << "1 - Inserir no início da lista\n";
+        std::cout << "2- Inserir no fim da lista\n";
+        std::cout << "3- Consultar a lista do início ao fim\n";
+        std::cout << "4- Consultar a lista do fim ao início\n";
+        std::cout << "5- Remover da lista\n";
+        std::cout << "6- Esvaziar a lista\n";
+        std::cout << "7- Sair\n";
 
-        printf("Digite uma opção: ");
-        scanf("%d", &escolha);
+        std::cout << "\nDigite sua opção: ";
+        std::cin >> opcao;
 
-        switch (escolha) {
-            case 1:
-                printf("Digite o valor para inserir no início: ");
-                scanf("%d", &valor);
-                lista = inserirNoInicio(lista, valor);
-                break;
-            case 2:
-                printf("Digite o valor para inserir no final: ");
-                scanf("%d", &valor);
-                lista = inserirNoFinal(lista, valor);
-                break;
-            case 3:
-                printf("Digite o valor para remover: ");
-                scanf("%d", &valor);
-                lista = removerElemento(lista, valor);
-                break;
-            case 4:
-                printf("Digite o valor para buscar: ");
-                scanf("%d", &valor);
-                No* encontrado = buscarElemento(lista, valor);
-                if (encontrado != NULL) {
-                    printf("Valor encontrado na lista.\n");
-                } else {
-                    printf("Valor não encontrado na lista.\n");
-                }
-                break;
-            case 5:
-                imprimirLista(lista);
-                break;
-            case 6:
-                printf("Encerrando o programa.\n");
-                break;
-            default:
-                printf("Opção inválida. Por favor, tente novamente.\n");
-                break;
+        if (opcao < 1 || opcao > 7)
+            std::cout << "Opção inválida!";
+
+        if (opcao == 1) {
+            std::cout << "Digite o número a ser inserido no início da lista: ";
+            auto *novo = new LISTA();
+            std::cin >> novo->numero;
+
+            if (inicio == nullptr) {
+                inicio = novo;
+                fim = novo;
+                novo->proximo = inicio;
+                novo->anterior = inicio;
+            } else {
+                novo->proximo = inicio;
+                inicio->anterior = novo;
+                novo->anterior = fim;
+                fim->proximo = novo;
+                inicio = novo;
+            }
+
+            std::cout << "Número inserido no início da lista com sucesso!";
         }
-    } while (escolha != 6);
 
-    // Liberar memória alocada
-    No* atual = lista;
-    while (atual != NULL) {
-        No* temp = atual;
-        atual = atual->proximo;
-        free(temp);
-    }
+        if (opcao == 2) {
+            std::cout << "Digite o número a ser inserido no fim da lista: ";
+            auto *novo = new LISTA();
+            std::cin >> novo->numero;
 
-    return 0;
+            if (inicio == nullptr) {
+                inicio = novo;
+                fim = novo;
+                novo->proximo = inicio;
+                novo->anterior = inicio;
+            } else {
+                fim->proximo = novo;
+                novo->anterior = fim;
+                fim = novo;
+                fim->proximo = inicio;
+                inicio->anterior = fim;
+            }
+
+            std::cout << "Número inserido no fim da lista com sucesso!";
+        }
+
+        if (opcao == 3) {
+            if (inicio == nullptr)
+                std::cout << "A lista está vazia!";
+            else {
+                std::cout << "\nConsultando a lista do início ao fim...\n";
+                auxiliar = inicio;
+
+                do {
+                    std::cout << auxiliar->numero << " ";
+                    auxiliar = auxiliar->proximo;
+                } while (auxiliar != inicio);
+            }
+        }
+
+        if (opcao == 4) {
+            if (inicio == nullptr) {
+                std::cout << "A lista está vazia!";
+            } else {
+                std::cout << "\nConsultando a lista do fim ao início...\n";
+                auxiliar = fim;
+
+                do {
+                    std::cout << auxiliar->numero << " ";
+                    auxiliar = auxiliar->anterior;
+                } while (auxiliar != fim);
+            }
+        }
+
+        if (opcao == 5) {
+            if (inicio == nullptr)
+                std::cout << "A lista está vazia!";
+            else {
+                std::cout << "\nDigite o elemento a ser removido: ";
+                std::cin >> numero;
+                auxiliar = inicio;
+                achou = 0;
+                int quantidade = 0;
+                auxiliar = inicio;
+
+                do {
+                    quantidade++;
+                    auxiliar = auxiliar->proximo;
+                } while (auxiliar != inicio);
+
+                int elemento = 1;
+
+                do {
+                    if (inicio == fim && inicio->numero == numero) {
+                        inicio = nullptr;
+                        delete (inicio);
+                        achou++;
+                    } else {
+                        if (auxiliar->numero == numero) {
+                            achou++;
+                            if (auxiliar == inicio) {
+                                inicio = auxiliar->proximo;
+                                inicio->anterior = fim;
+                                fim->proximo = inicio;
+                                delete (auxiliar);
+                                auxiliar = inicio;
+                            } else if (auxiliar == fim) {
+                                fim = fim->anterior;
+                                fim->proximo = inicio;
+                                inicio->anterior = fim;
+                                delete (auxiliar);
+                                auxiliar = nullptr;
+                            } else {
+                                auxiliar->anterior->proximo = auxiliar->proximo;
+                                auxiliar->proximo->anterior = auxiliar->anterior;
+                                LISTA *auxiliar2;
+                                auxiliar2 = auxiliar;
+                                auxiliar = auxiliar->proximo;
+                                delete (auxiliar2);
+                            }
+                        } else {
+                            auxiliar = auxiliar->proximo;
+                        }
+                    }
+
+                    elemento++;
+                } while (elemento <= quantidade);
+
+                if (achou == 0)
+                    std::cout << "Número não encontrado.";
+                else if (achou == 1)
+                    std::cout << "Número removido uma vez.";
+                else
+                    std::cout << "Número removido " << achou << " vezes.";
+            }
+        }
+
+        if (opcao == 6) {
+            if (inicio == nullptr)
+                std::cout << "A lista está vazia!";
+            else {
+                auxiliar = inicio;
+
+                do {
+                    inicio = inicio->proximo;
+                    delete (auxiliar);
+                    auxiliar = inicio;
+                } while (auxiliar != fim);
+
+                delete (fim);
+                inicio = nullptr;
+                std::cout << "A lista esvaziada com sucesso!";
+            }
+        }
+
+        getch();
+    } while (opcao != 7);
 }
